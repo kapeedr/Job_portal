56341528a03ffda619a489b0e54fed8264be5f50
Deepak Kumar <deepak180222@gmail.com>
2022-08-05 10:20:29 +0530

    start using rest api

--- Server/routes/jobsRoute.js
+++ Server/routes/jobsRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Job = require('../model/jobModel');
+const Job = require('../Server/model/jobModel');
 router.get("/getalljobs", async(req,res) => {
     try{
         const jobs = await Job.find()
